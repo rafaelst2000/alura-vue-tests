2b11de89d4a1cb3093cf2b91f8698c226981337e
Rafael Trevisan <rafael.trevisan@goclin.com>
2021-03-02 16:13:35 -0300

    first commit

---
+++ tests/unit/Lance.spec.js
@@ -0,0 +1,74 @@
+import Lance from '@/components/Lance'
+import { mount } from '@vue/test-utils'
+describe('um lance sem um valor minimo', () => {
+  test('não aceita lance com valor menor que zero', () => {
+    const wrapper = mount(Lance)
+    const input = wrapper.find('input')
+    input.setValue(-100)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    expect(lancesEmitidos).toBeUndefined()
+  })
+
+  test('emite um lance quando o valor é maior do que zero', () => {
+    const wrapper = mount(Lance)
+    const input = wrapper.find('input')
+    input.setValue(100)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    expect(lancesEmitidos).toHaveLength(1)
+  })
+
+  test('emite o valor esperado de um lance válido', () => {
+    const wrapper = mount(Lance)
+    const input = wrapper.find('input')
+    input.setValue(100)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    const lance = parseInt(lancesEmitidos[0][0])
+    expect(lance).toBe(100)
+  })
+})
+describe('um lance com um valor minimo', () => {
+  test('todos os lances devem possuir um valor maior que o minimo informado', () => {
+    const wrapper = mount(Lance, {
+      propsData: {
+        lanceMinimo: 300
+      }
+    })
+    const input = wrapper.find('input')
+    input.setValue(400)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    expect(lancesEmitidos).toHaveLength(1)
+  })
+
+  test('emite o valor esperado de um lance valido', () => {
+    const wrapper = mount(Lance, {
+      propsData: {
+        lanceMinimo: 300
+      }
+    })
+    const input = wrapper.find('input')
+    input.setValue(400)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    const valorDoLance = parseInt(lancesEmitidos[0][0])
+    expect(valorDoLance).toBe(400)
+  })
+
+  test('não são aceitos lances com valores menores do que o mínimo informado', async () => {
+    const wrapper = mount(Lance, {
+      propsData: {
+        lanceMinimo: 300
+      }
+    })
+    const input = wrapper.find('input')
+    input.setValue(100)
+    wrapper.trigger('submit')
+    await wrapper.vm.$nextTick() // espera que o dom seja atualizado
+    const msgErro = wrapper.find('p.alert').element.textContent
+    const msgEsperada = 'O valor mínimo para o lance é de R$ 300'
+    expect(msgErro).toContain(msgEsperada)
+  })
+})
